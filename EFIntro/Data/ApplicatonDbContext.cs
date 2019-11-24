05a817252a6cbc8f9b2739fbad70178794526b70
Allison Patton <allison.tpttn@gmail.com>
2019-11-24 15:22:33 -0600

    working Entity Framework code-along

---
+++ EFIntro/Data/ApplicatonDbContext.cs
@@ -0,0 +1,19 @@
+﻿//using DepartmentsEmployeesEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFIntro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFIntro.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // TODO: Add "DbSet<T>"s for each table here...
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
